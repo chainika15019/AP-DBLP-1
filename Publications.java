b4321dd0279beb874e2ed2a69d31e3ae5784e71b
Utsav <utsav15110@iiitd.ac.in>
2016-11-08 19:19:01 +0530

    Made Publications class: It contains the useful details of publications.

---
+++ Publications.java
@@ -0,0 +1,18 @@
+class Publications {
+	private ArrayList <String> authors;
+	private String title;
+	private String pages;
+	private int volume;
+	private String journal;
+	private String ee;
+	private String url;
+
+	public ArrayList <String> getAuthor()
+	{
+		return authors;
+	}
+
+	/*
+		Getter and setters to be written.
+	*/
+}
