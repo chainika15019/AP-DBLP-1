1b32bdf09223202bba5a28b1df84be6013540237
Utsav <utsav15110@iiitd.ac.in>
2016-11-08 19:17:37 +0530

    Made the basic structure of Parser class

---
+++ Parser.java
@@ -0,0 +1,27 @@
+// This will be our main class.
+
+class Parser {
+	ArrayList <Publications> ListPublications;
+	ArrayList <Authors> ListAuthors;
+	int whichquery;
+
+	public ArrayList <Publications> getListPub()
+	{
+		return ListPublications;
+	}
+
+	public ArrayList <Authors> getListAuthors()
+	{
+		return ListAuthors;
+	}
+
+	public int getwhichquery()
+	{
+		return whichquery;
+	}
+
+	public static void main(String args[])
+	{
+		// to be done.
+	}
+}
