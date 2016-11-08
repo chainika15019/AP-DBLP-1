e631e6d9d3c38d5076dd874cad2f9184119afe09
Utsav <utsav15110@iiitd.ac.in>
2016-11-08 19:19:52 +0530

    Made the author class with its basic functions.

---
+++ Authors.java
@@ -0,0 +1,18 @@
+class Authors {
+	private ArrayList <String> names;
+	private int mappedVal;
+
+	public void setNames(ArrayList <String> name)
+	{
+		names = name;
+	}
+
+	public int getMappedVal()
+	{
+		return mappedVal;
+	}
+
+	/*
+		Wrote the basic structure of this class.
+	*/
+}
