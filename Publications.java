4ef61a3f4d36434e5a3c4377c6581ff68db2d82e
Utsav Rohilla <utsav-15110@users.noreply.github.com>
2016-11-24 19:18:31 +0530

    Updated Publications.java
    Still have some error.!

--- Publications.java
+++ Publications.java
@@ -1,4 +1,4 @@
-class Publications {
+class Main {
 	private ArrayList <String> authors;
 	private String title;
 	private String pages;
@@ -41,4 +41,40 @@ class Publications {
 	{
 		return url;
 	}
+	
+	public void setAuthor(ArrayList <String> authors_input)
+	{
+		authors = authors_input;
+	}
+	
+	public void setTitle(String title_input)
+	{
+		title = title_input;
+	}
+	
+	public void setPages(String pages_input)
+	{
+		pages = pages_input;
+	}
+	
+	public void setVolume(int volume_input)
+	{
+		volume = volume_input;
+	}
+	
+	public void setJournal(String journal_input)
+	{
+		journal = journal_input;
+	}
+	
+	public void setEE(String ee_input)
+	{
+		ee = ee_input;
+	}
+	
+	public void setURL(String url_input)
+	{
+		url = url_input;
+	}
+
 }
