357e9fe1aa8982236db304536bfef442f76b2a14
Utsav Rohilla <utsav-15110@users.noreply.github.com>
2016-11-28 22:43:59 +0530

    Current Authors.java

--- Authors.java
+++ Authors.java
@@ -1,10 +1,52 @@
-class Authors {
-	private ArrayList <String> names;
-	private int mappedVal;
+import java.util.*;
 
-	public void setNames(ArrayList <String> name)
+public class Authors {
+	static private int mval = -1;
+	// static Has
+	private ArrayList <String> names;
+	private int mappedVal = -1;
+	private int numPub = 0;
+	private int Val=1;
+
+	Authors()
 	{
-		names = name;
+		mval++;
+		names = new ArrayList<String>();
+	}
+
+	public int getVal()
+	{
+		return Val;
+	}
+
+	public void setVal(int val)
+	{
+		Val = val;
+	}
+
+	public int getNumPub()
+	{
+		return numPub;
+	}
+
+	public void addNumPub()
+	{
+		numPub++;
+	}
+
+	public void addAuthor(String name)
+	{
+		names.add(name);
+	}
+
+	public void setMappedVal()
+	{
+		mappedVal = mval;
+	}
+
+	public ArrayList <String> getNames()
+	{
+		return names;
 	}
 
 	public int getMappedVal()
@@ -12,7 +54,6 @@ class Authors {
 		return mappedVal;
 	}
 
-	/*
-		Wrote the basic structure of this class. 
-	*/
+
+
 }
