d03350259c6d428704c62b1002460e5d3671b58e
Utsav Rohilla <utsav-15110@users.noreply.github.com>
2016-11-29 17:37:29 +0530

    Update Query1.java

--- Query1.java
+++ Query1.java
@@ -1,10 +1,20 @@
+import java.util.*;
+import java.io.IOException;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+
+
 public class Query1 {
 
-	Query1()
+	Query1() throws IOException, SAXException
 	{
+		System.setProperty("jdk.xml.entityExpansionLimit", "0");
+
 
 		XMLReader p = XMLReaderFactory.createXMLReader();
-		p.setContentHandler(new Parser());
+		p.setContentHandler(new Parser("Author","Sanjeev Saxena","","",""));
 		p.parse("dblp.xml");
 
 
