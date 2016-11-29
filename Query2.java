8457c98602a0c0bd5f8f0432bc4772354b9ccb30
Utsav Rohilla <utsav-15110@users.noreply.github.com>
2016-11-29 21:11:09 +0530

    Update Query2.java

--- Query2.java
+++ Query2.java
@@ -4,24 +4,25 @@ public class Query2 {
 
 	private ArrayList <Authors> authors = PREPROCESS.getAuthors();
 	private int count = 0;
+	ArrayList <String> names = new ArrayList <String>();
 
 
 	Query2(int minPub)
 	{
-		int max = authors.get(0).getNumPub();
-		int j = 0;
+
 		for(int i = 0 ; i < authors.size() ; i++)
 		{
-			if(authors.get(i).getNumPub() > max)
+			if(authors.get(i).getNumPub() >= minPub)
 			{
-				max = authors.get(i).getNumPub();
-				j = i;
-				// System.out.println(authors.get(i).getNames().get(0));
-				count ++;
+				names.add(authors.get(i).getNames().get(0));
 			}
 		}
-		System.out.println(max);
-		System.out.println(authors.get(j).getNames().get(0));
+
+	}
+	
+	public ArrayList <String> getNames()
+	{
+		return names;
 	}
 
 }
