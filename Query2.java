038adb54092803d22dc0dcaebd6c44c69851468b
Utsav Rohilla <utsav-15110@users.noreply.github.com>
2016-11-29 17:37:48 +0530

    Update Query2.java

--- Query2.java
+++ Query2.java
@@ -8,17 +8,20 @@ public class Query2 {
 
 	Query2(int minPub)
 	{
-
+		int max = authors.get(0).getNumPub();
+		int j = 0;
 		for(int i = 0 ; i < authors.size() ; i++)
 		{
-			if(authors.get(i).getNumPub() >= minPub)
+			if(authors.get(i).getNumPub() > max)
 			{
-				System.out.println(authors.get(i).getNames().get(0));
+				max = authors.get(i).getNumPub();
+				j = i;
+				// System.out.println(authors.get(i).getNames().get(0));
 				count ++;
 			}
 		}
-		System.out.println(count);
-
+		System.out.println(max);
+		System.out.println(authors.get(j).getNames().get(0));
 	}
 
 }
